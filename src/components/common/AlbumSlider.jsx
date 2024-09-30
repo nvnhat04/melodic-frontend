c4eab4a0284916babdecf5435e1c20d3967f7d4b
longsobad <nguyenlongdz2004@gmail.com>
2024-09-30 23:39:42 +0700

    feat: add AlbumSlider

---
+++ src/components/common/AlbumSlider.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import AlbumCard from "./AlbumCard/AlbumCard.jsx";
+import { Box } from "@mui/material";
+import "swiper/swiper-bundle.css";
+
+const AlbumSlider = ({ AlbumList}) => {
+  return (
+    <Box style={{ maxWidth: "100%" }}>
+      <Swiper
+        spaceBetween={1}
+        slidesPerView={4}
+        slidesPerGroup={4}
+        grabCursor={true}
+        direction="horizontal"
+        observer={true}
+      >
+        {AlbumList.map((album) => (
+          <SwiperSlide key={album.id}>
+            <AlbumCard album={album}  />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  );
+};
+
+export default AlbumSlider;
