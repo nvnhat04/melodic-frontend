ae7b022f773d6f555e6a93dfa58b537fc733311e
nvnhat04 <nvnhat04324@gmail.com>
2024-10-22 00:22:22 +0700

    feat: refactoring folder AlbumCard

--- src/components/common/AlbumCard.jsx
+++ src/components/common/AlbumCard.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const AlbumCard = ({ album }) => {
   return (
     <Link to={`/album/${album.id}`} style={{ textDecoration: "none" }}>
-      <Card sx={{ overflow: "hidden", border: 'none', boxShadow: 'none' }}> 
+      <Card sx={{ overflow: "hidden", border: 'none', boxShadow: 'none', padding: '10px' }}> 
         <CardMedia
           component="img"
           image={album.cover}
