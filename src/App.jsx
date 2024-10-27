f411141b17cfc1bbca541ffef16aae24a3176dda
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2024-10-27 13:02:15 +0700

    fix: rename ArtistDashboard to ArtistProfile and update imports
    Renamed the ArtistDashboard component to ArtistProfile to better reflect its purpose. Updated all relevant imports to ensure consistency across the application and avoid potential import errors.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import ArtistDashboard from './pages/ArtistProfile';
+import ArtistProfile from './pages/ArtistProfile';
 import PlayScreen from './pages/PlayScreen';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes className="App">
           <Route path="/" element ={<HomePage/>}></Route>
-          <Route path="/artist-profile" element ={<ArtistDashboard/>}></Route>
+          <Route path="/artist-profile" element ={<ArtistProfile/>}></Route>
           <Route path="/play-screen" element ={<PlayScreen/>}></Route>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} /> {/* Consistent component name */}
