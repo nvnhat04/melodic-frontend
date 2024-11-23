fdb599bd95d827698ff97f5af504487a6265443a
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2024-11-23 22:34:27 +0700

    style: add new fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,86 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from './pages/HomePage';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import ArtistProfile from './pages/ArtistProfile';
-import ArtistDashboard from './pages/ArtistDashboard';
-import AdminDashboard from './pages/AdminDashboard';
-import PlayScreen from './pages/PlayScreen';
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ArtistProfile from "./pages/ArtistProfile";
+import ArtistDashboard from "./pages/ArtistDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import PlayScreen from "./pages/PlayScreen";
 import ArtistManageMerchandise from "./pages/ArtistManageMerchandise";
 import ArtistAddNewMerchandise from "./pages/ArtistAddNewMerchandise";
 
-import Playlist from './pages/Playlist';
-import MainLayout from './components/layout/MainLayout';
-import ShopPage from './pages/ShopPage';
-import MerchandiseDetail from './pages/MerchandiseDetail';
-import ManageRequest from './components/Admin/ManageRequest';
-import ManageUsers from './components/Admin/ManageUsers';
-import ManageTracks from './components/Admin/ManageTracks';
-import ManagePlaylists from './components/Admin/ManagePlaylists';
-import CartPage from './pages/CartPage';
-import MerchSearch from './pages/MerchSearch';
+import Playlist from "./pages/Playlist";
+import MainLayout from "./components/layout/MainLayout";
+import ShopPage from "./pages/ShopPage";
+import MerchandiseDetail from "./pages/MerchandiseDetail";
+import ManageRequest from "./components/Admin/ManageRequest";
+import ManageUsers from "./components/Admin/ManageUsers";
+import ManageTracks from "./components/Admin/ManageTracks";
+import ManagePlaylists from "./components/Admin/ManagePlaylists";
+import CartPage from "./pages/CartPage";
+import MerchSearch from "./pages/MerchSearch";
 
-import Album from './pages/Album';
-import Libraries from './pages/Libraries';
-import TrackDetail from './pages/TrackDetail';
+import Album from "./pages/Album";
+import Libraries from "./pages/Libraries";
+import TrackDetail from "./pages/TrackDetail";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [ "Neue Helvetica Condensed BQ", "Inter", "san-serif"].join(","),
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes className="App">
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="artists" element={<Libraries type="artists" />} />
-          <Route path="albums" element={<Libraries type="albums" />} />
-          <Route path="playlists" element={<Libraries type="playlists" />} />
-          <Route path='tracks' element={<Libraries type="tracks" />} />
-          <Route path='track/:id' element={<TrackDetail />} />
-          <Route path='playlist/:id' element={<Playlist />} />
-          <Route path='artist/:id' element={<ArtistProfile />} />
-          <Route path='album/:id' element={<Album />} />
-        </Route>
-          <Route path="/home" element ={<HomePage/>}></Route>
-          <Route path="/artist-profile" element ={<ArtistProfile/>}></Route>
-          <Route path="/play-screen" element ={<PlayScreen/>}></Route>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes className="App">
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="artists" element={<Libraries type="artists" />} />
+            <Route path="albums" element={<Libraries type="albums" />} />
+            <Route path="playlists" element={<Libraries type="playlists" />} />
+            <Route path="tracks" element={<Libraries type="tracks" />} />
+            <Route path="track/:id" element={<TrackDetail />} />
+            <Route path="playlist/:id" element={<Playlist />} />
+            <Route path="artist/:id" element={<ArtistProfile />} />
+            <Route path="album/:id" element={<Album />} />
+          </Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/artist-profile" element={<ArtistProfile />}></Route>
+          <Route path="/play-screen" element={<PlayScreen />}></Route>
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} /> 
+          <Route path="/register" element={<Register />} />
           <Route path="/artist" element={<ArtistDashboard />} />
           <Route path="/test" element={<Playlist />}></Route>
-          <Route path="/register" element={<Register />} /> 
-          
-           <Route path="/artist" element={<ArtistDashboard />}>
-              <Route
-                path="merchandise"
-                element={<ArtistManageMerchandise />}
-              ></Route>
-              <Route
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/artist" element={<ArtistDashboard />}>
+            <Route
+              path="merchandise"
+              element={<ArtistManageMerchandise />}
+            ></Route>
+            <Route
               path="upload-merchandise"
-              element={<ArtistAddNewMerchandise/>}
-              ></Route>
+              element={<ArtistAddNewMerchandise />}
+            ></Route>
           </Route>
-            
-          <Route path="/admin" element={<AdminDashboard />} >
-            <Route path="users" element={<ManageUsers/>} />
-            <Route index element={<ManageRequest/>} />
-            <Route path="tracks" element={<ManageTracks/>} />
-            <Route path="playlists" element={<ManagePlaylists/>} />
+
+          <Route path="/admin" element={<AdminDashboard />}>
+            <Route path="users" element={<ManageUsers />} />
+            <Route index element={<ManageRequest />} />
+            <Route path="tracks" element={<ManageTracks />} />
+            <Route path="playlists" element={<ManagePlaylists />} />
           </Route>
-          <Route path="/shop" element={<ShopPage/>} />
-          <Route path = "/merchandise/:id"  element={<MerchandiseDetail/>} />
-          <Route path = "/shop/cart" element={<CartPage/>} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/merchandise/:id" element={<MerchandiseDetail />} />
+          <Route path="/shop/cart" element={<CartPage />} />
           <Route path="/merch-search" element={<MerchSearch />} />
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/components/common/MerchandiseCard.jsx
+++ src/components/common/MerchandiseCard.jsx
@@ -3,57 +3,49 @@ import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
-const theme = createTheme({
-  typography: {
-    fontFamily: ["Neue Helvetica Condensed BQ", "san-serif"].join(","),
-  },
-});
-
-const MerchandiseCard = ({merchandise}) => {
+const MerchandiseCard = ({ merchandise }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Paper
+    <Paper
+      sx={{
+        width: "80%",
+        textAlign: "center",
+        fontSize: "1em",
+        padding: 2,
+        overflow: "hidden",
+        cursor: "pointer",
+        "&:hover img": {
+          opacity: 0.7,
+        },
+        border: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Box
+        component="img"
+        src={merchandise.image}
+        alt={merchandise.name}
         sx={{
-          width: "80%",
-          textAlign: "center",
+          width: "100%",
+          objectFit: "cover",
+          aspectRatio: "1/1",
+        }}
+      />
+      <Typography
+        sx={{
           fontSize: "1em",
-          padding: 2,
-          overflow: "hidden",
-          cursor: "pointer",
-          "&:hover img": {
-            opacity: 0.7,
-          },
-          border: "none",
-          boxShadow: "none",
+          fontWeight: "600",
+          lineHeight: "1.4",
         }}
       >
-        <Box
-          component="img"
-          src={merchandise.image}
-          alt={merchandise.name}
-          sx={{
-            width: "100%",
-            objectFit: "cover",
-            aspectRatio: "1/1",
-          }}
-        />
-        <Typography
-          sx={{
-            fontSize: "1em",
-            fontWeight: "600",
-            lineHeight: "1.4",
-          }}
-        >
-          {merchandise.name}
-        </Typography>
-        <Typography
-          mt={1}
-          sx={{ fontSize: "0.8em", fontWeight: "600", color: "#1a1a1a" }}
-        >
-          ${merchandise.price}
-        </Typography>
-      </Paper>
-    </ThemeProvider>
+        {merchandise.name}
+      </Typography>
+      <Typography
+        mt={1}
+        sx={{ fontSize: "0.8em", fontWeight: "600", color: "#1a1a1a" }}
+      >
+        ${merchandise.price}
+      </Typography>
+    </Paper>
   );
 };
 
