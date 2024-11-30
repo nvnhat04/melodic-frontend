9226cb8ab6d29d475faf04a187c819600cc5be24
nvnhat04 <nvnhat04324@gmail.com>
2024-11-30 17:22:01 +0700

    feat: create addNewAlbum component and artist API

---
+++ src/api/modules/artist.api.js
@@ -0,0 +1,66 @@
+import Client from "../client.api";
+
+const Artist = {
+    async addAlbum(data) {
+        if(data) {
+            console.log('Data:', data);
+        } else {
+            console.log('No data received');
+        }
+
+        try {
+            const response = await Client.post('/artist/add-album', data,  {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            return response;
+        } catch (error) {
+            if (error.response) {
+                return error.response;
+            }
+        }
+    },
+    async getAllAlbums(id) {
+        // console.log('Artist ID:', id);
+        try {
+            const response = await Client.get(`/artist/${id}/albums`);
+            return response;
+        } catch (error) {
+            if (error.response) {
+                return error.response;
+            }
+        }
+    },
+    async getRequestedTracks(id) {
+        try {
+            const response = await Client.get(`/artist/pending/${id}`);
+            return response;
+        } catch (error) {
+            if (error.response) {
+                return error.response;
+            }
+        }
+    },
+    async approveCollaboration(data) {
+        try {
+            const response = await Client.put('/artist/approve-track', data);
+            return response;
+        } catch (error) {
+            if (error.response) {
+                return error.response;
+            }
+        }
+    },
+    async rejectCollaboration(data) {
+        try {
+            const response = await Client.put('/artist/reject-track', data);
+            return response;
+        } catch (error) {
+            if (error.response) {
+                return error.response;
+            }
+        }
+    },
+};
+export default Artist;
