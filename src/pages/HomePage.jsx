54482875069825aa6188d73ba75638397dec0a2c
thuthuy <125456003+tthuy123@users.noreply.github.com>
2024-12-06 15:10:46 +0700

    feat: integrate playlist and music APIs to fetch and display various playlists and tracks on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,87 +2,135 @@ import React, { useEffect, useState } from "react";
 import AlbumSlider from "../components/common/Slider";
 import { Box, Stack, Typography } from "@mui/material";
 import Container from "../components/common/Container";
-import MusicPlayer from "../components/common/MusicPlayer";
+import PlaylistCard from "../components/common/PlaylistCard";
+import TrackSlider from "../components/HomePage/TrackSlider";
 
-const mockAlbums = [
-  {
-    id: 1,
-    name: "Greatest Hits",
-    cover:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNcqqlxSjVVcNbnAYS-mvZIypKavEO3edz6g&s",
-  },
-  {
-    id: 2,
-    name: "Summer Vibes",
-    artist: "long",
-    cover:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbdUlAVv8BhOstJLt47s3uceLIQQg-E2JDVg&s",
-  },
-  {
-    id: 3,
-    name: "Acoustic Sessions",
-    cover:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ8D6I_OM3e_cbZ5zhovzOMAm8BZxlsxxVsw&s",
-  },
-  {
-    id: 4,
-    name: "Live in Concert",
-    cover:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTf7Tn0Z1nA8ejs7ErQww1fWN_QGva57daeA&s",
-  },
-  {
-    id: 5,
-    name: "Classical Essentials",
-    cover:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8cxzBS2Ms5_7xSoZiHqSre5UJ43GfuQLdFw&s",
-  },
-  {
-    id: 6,
-    name: "Classical Essentials",
-    cover:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8gjZolciMa0C4cL6096_Oapvt7W6GIrsrEA&s",
-  },
-  {
-    id: 7,
-    name: "Classical Essentials",
-    cover:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj4l8txJSFCnQwOwJ5P-X8Obf9R0LJ8zBlkg&s",
-  },
-];
+import playlistAPI from "../api/modules/playlist.api";
+import MusicAPI from "../api/modules/music.api";
+import FavoriteAPI from "../api/modules/favorite.api"
+import { useSelector, useDispatch } from "react-redux";
+import { setListFavorites } from "../redux/store";
+
+
 
 function HomePage() {
   const [popularAlbums, setPopularAlbums] = useState([]);
-  const [dailyTopHits, setDailyTopHits] = useState([]);
+  const [dailyTopHits, setDailyTopHits] = useState("");
+  const [melodicTopTracks, setMelodicTopTracks] = useState("");
+  const [dailyTopFavorites, setDailyTopFavorites] = useState("");
   const [newReleases, setNewReleases] = useState([]);
+  const [moods, setMoods] = useState([]);
+  const [topArtists, setTopArtists] = useState([]);
+  const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    setPopularAlbums(mockAlbums);
-    setDailyTopHits(mockAlbums);
-    setNewReleases(mockAlbums);
+    const fetchData = async () => {
+      try {
+        const favoriteResponse = await FavoriteAPI.getListFavorites(token);
+          dispatch(setListFavorites(favoriteResponse))
+
+        const DailyTopHitsPlaylist = await playlistAPI.getPlaylistById(
+          1,
+          token
+        );
+        setDailyTopHits(DailyTopHitsPlaylist);
+
+        const topTracks = await playlistAPI.getPlaylistById(2, token);
+        setMelodicTopTracks(topTracks);
+
+        const dailyTopFavorites = await playlistAPI.getPlaylistById(47, token);
+        setDailyTopFavorites(dailyTopFavorites);
+
+        const newReleases = await MusicAPI.getNewReleases();
+        setNewReleases(newReleases);
+
+        const popularAlbums = await MusicAPI.getTopAlbums();
+        setPopularAlbums(popularAlbums);
+
+        const topArtists = await MusicAPI.getTopArtists();
+        setTopArtists(topArtists);
+
+        const publicPlaylists = await MusicAPI.getPublicPlaylists();
+        setMoods(publicPlaylists);
+
+  
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
   }, []);
+
+
 
   return (
     <Stack
-          spacing={7}
-          sx={{ width: "75vw", margin: "2rem auto", color: "#fff" }}
+      spacing={5}
+      sx={{ width: "75vw", margin: "2rem auto", color: "#fff" }}
+    >
+      <Container header="Discover" sx={{
+        height: "50vh"
+      }}>
+        <Box
+          sx={{
+            display: "flex", // Sử dụng flexbox để xếp theo hàng ngang
+            gap: "10px",
+            width: "100%", // Đảm bảo chiều rộng của Box là 100%
+          }}
         >
-          {popularAlbums.length > 0 && (
-            <Container header="Popular Albums">
-              <AlbumSlider list={popularAlbums} type={"Album"} />
-            </Container>
-          )}
-          {dailyTopHits.length > 0 && (
-            <Container header="Daily Top Hits">
-              <AlbumSlider list={dailyTopHits} type={"Album"} />
-            </Container>
-          )}
-          {newReleases.length > 0 && (
-            <Container header="New Releases">
-              <AlbumSlider list={newReleases} type={"Album"} />
-            </Container>
-          )}
-          <Box sx={{ height: "13%", width: "100%" }}></Box>
-        </Stack>
+          <Box
+            sx={{
+              flex: "1", // Chia đều không gian cho hai Card
+              maxWidth: "33%", // Đảm bảo mỗi Card không vượt quá 50% chiều rộng
+            }}
+          >
+            <PlaylistCard playlist={dailyTopHits} />
+          </Box>
+          <Box
+            sx={{
+              flex: "1",
+              maxWidth: "33%",
+            }}
+          >
+            <PlaylistCard playlist={melodicTopTracks} />
+          </Box>
+          <Box
+            sx={{
+              flex: "1",
+              maxWidth: "33%",
+            }}
+          >
+            <PlaylistCard playlist={dailyTopFavorites} />
+          </Box>
+        </Box>
+      </Container>
+
+      <Container header='New Releases'>
+        <TrackSlider tracks={newReleases}/>
+      </Container>
+
+      {popularAlbums.length > 0 && (
+        <Container header="Popular Albums">
+          <AlbumSlider list={popularAlbums} type={"Album"} />
+        </Container>
+      )}
+
+      {moods.length > 0 && (
+        <Container header="Moods">
+          <AlbumSlider list={moods} type={"Album"} />
+        </Container>
+      )}
+     
+
+     
+      {/* {newReleases.length > 0 && (
+        <Container header="Moods">
+          <AlbumSlider list={newReleases} type={"Album"} />
+        </Container>
+      )} */}
+      <Box sx={{ height: "17%", width: "100%" }}></Box>
+    </Stack>
   );
 }
 
