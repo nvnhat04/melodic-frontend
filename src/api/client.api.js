2cd42a8624b2ea9fd4bda408d3758dae392b5990
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2024-12-07 20:25:32 +0700

    feat(api): add token handling in request interceptor and improve error handling in response interceptor

--- src/api/client.api.js
+++ src/api/client.api.js
@@ -1,22 +1,42 @@
-import axios from 'axios';
+import axios from "axios";
+import store from "../redux/store";
 
 const Client = axios.create({
-    baseURL:'http://localhost:3000/api/v1',
-    headers: {
-    'content-type': 'application/json',
-    },
-    });
-    Client.interceptors.request.use(async (config) => {
-    // Handle token here ...
+  baseURL: "http://localhost:3000/api/v1",
+  headers: {
+    "content-type": "application/json",
+  },
+});
+
+Client.interceptors.request.use(
+  async (config) => {
+    const state = store.getState();
+    const token = state.auth.token; 
+    if (token) {
+      config.headers.token = `Bearer ${token}`;
+      console.log("Authorization Header:", config.headers.Authorization);
+    } else {
+      console.warn("No token provided");
+    }
+
     return config;
-})
-Client.interceptors.response.use((response) => {
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+Client.interceptors.response.use(
+  (response) => {
     if (response && (response.data || response.data === 0)) {
-    return response.data;
+      return response.data;
     }
     return response;
-    }, (error) => {
+  },
+  (error) => {
     // Handle errors
     throw error;
-});
+  }
+);
+
 export default Client;
