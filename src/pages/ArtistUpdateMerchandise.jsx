469c4f3ab06098b5dfbc22a18fc8b2c264400724
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2024-12-08 22:07:11 +0700

    feat(ArtistUpdateMerchandise): add merchandise update form

---
+++ src/pages/ArtistUpdateMerchandise.jsx
@@ -0,0 +1,217 @@
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import Grid from "@mui/material/Grid2";
+import MenuItem from "@mui/material/MenuItem";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import Select from "@mui/material/Select";
+import Typography from "@mui/material/Typography";
+import MerchandiseCard from "../components/common/MerchandiseCard";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { useParams } from "react-router-dom";
+import MerchandiseApi from "../api/modules/merchandise.api";
+
+const ArtistUpdateMerchandise = () => {
+  const { id: merchandiseId } = useParams();
+
+  const [merchandise, setMerchandise] = useState({
+    name: "",
+    category: "",
+    price: "",
+    quantityInStock: "",
+    description: "",
+    image:
+      "https://shop.thenbhd.com/cdn/shop/products/TheNBHD_ILoveYouD2C_1024x1024@2x.png?v=1681914792",
+    relatedAlbum: "",
+  });
+
+  useEffect(() => {
+    const fetchMerchandise = async () => {
+      try {
+        const data = await MerchandiseApi.getMerchandiseById(merchandiseId);
+        setMerchandise({
+          name: data.name || "",
+          category: data.category || "",
+          price: data.price || "",
+          quantityInStock: data.quantityInStock || "",
+          description: data.description || "",
+          image: data.image || "",
+          relatedAlbum: data.relatedAlbum || "",
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMerchandise();
+  }, []);
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setMerchandise((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(merchandise);
+    // TODO: Add API call to update merchandise
+  };
+
+  return (
+    <Box display="flex" flexDirection="column">
+      <Typography
+        component="h1"
+        variant="h5"
+        sx={{ textTransform: "uppercase", fontWeight: "bold" }}
+      >
+        Add new merchandise
+      </Typography>
+      <Box display="flex" flexflow="row wrap">
+        <Grid
+          component="form"
+          onSubmit={handleSubmit}
+          flex="1 1 50%"
+          container
+          columns={2}
+          p={2}
+          columnSpacing={2}
+          rowSpacing={1}
+          mr={5}
+          sx={{ "& .MuiFormLabel-root": { color: "black" } }}
+        >
+          {/* Merchandise Form */}
+          <Grid size={2}>
+            <FormControl variant="filled" fullWidth required>
+              <FormLabel component="h2" variant="h2">
+                Merchandise Name
+              </FormLabel>
+              <OutlinedInput
+                name="name"
+                value={merchandise.name}
+                fullWidth
+                onChange={handleChange}
+              />
+            </FormControl>
+          </Grid>
+          <Grid size={2}>
+            <FormControl fullWidth required>
+              <FormLabel>Category</FormLabel>
+              <Select
+                id="category"
+                name="category"
+                onChange={handleChange}
+                value={merchandise.category || ""}
+              >
+                <MenuItem value="Digital Album">Digital Album</MenuItem>
+                <MenuItem value="Physical Album">Physical Album</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid size={2}>
+            <FormControl fullWidth required>
+              <FormLabel>Related Album</FormLabel>
+              <Select
+                id="relatedAlbum"
+                name="relatedAlbum"
+                onChange={handleChange}
+                value={merchandise.relatedAlbum || ""}
+              >
+                <MenuItem value="Chip Chrome">Chip Chrome</MenuItem>
+                <MenuItem value="Sweater Weather">Sweater Weather</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid size={{ xs: 2, sm: 2, md: 1 }}>
+            <FormControl variant="filled" fullWidth required>
+              <FormLabel>Price</FormLabel>
+              <OutlinedInput
+                name="price"
+                type="number"
+                value={merchandise.price || ""}
+                fullWidth
+                onChange={handleChange}
+              />
+            </FormControl>
+          </Grid>
+          <Grid size={{ xs: 2, sm: 2, md: 1 }}>
+            <FormControl variant="filled" fullWidth required>
+              <FormLabel>Quantity In Stock</FormLabel>
+              <OutlinedInput
+                name="quantityInStock"
+                type="number"
+                value={merchandise.quantityInStock || ""}
+                fullWidth
+                onChange={handleChange}
+              />
+            </FormControl>
+          </Grid>
+          <Grid size={2}>
+            <FormControl variant="filled" fullWidth>
+              <FormLabel>Description</FormLabel>
+              <OutlinedInput
+                value={merchandise.description || ""}
+                name="description"
+                multiline
+                minRows={4}
+                fullWidth
+                onChange={handleChange}
+              />
+            </FormControl>
+          </Grid>
+          <Grid size={2}>
+            <FormControl variant="filled" fullWidth>
+              <Button
+                component="label"
+                variant="contained"
+                startIcon={<CloudUploadIcon />}
+                sx={{ backgroundColor: "#564c4d" }}
+              >
+                Upload image
+              </Button>
+            </FormControl>
+          </Grid>
+          <Grid size={2} variant="filled">
+            <Box
+              mt={2}
+              display="flex"
+              flexDirection="row"
+              justifyContent="center"
+              gap={2}
+            >
+              <FormControl fullWidth>
+                <Button type="submit" variant="contained">
+                  Publish
+                </Button>
+              </FormControl>
+              <FormControl fullWidth>
+                <Button type="reset" variant="outlined">
+                  Cancel
+                </Button>
+              </FormControl>
+            </Box>
+          </Grid>
+        </Grid>
+        <Box
+          flex="1 1 50%"
+          display={{ xs: "none", sm: "none", md: "flex" }}
+          alignItems="center"
+          flexDirection="column"
+          p={4}
+        >
+          {/* Preview */}
+          <Typography variant="h4" component="h2" alignSelf="start">
+            Preview
+          </Typography>
+          <Typography component="p" alignSelf="start">
+            This is how your merchandise appears in Artist Shop.
+          </Typography>
+          <MerchandiseCard merchandise={merchandise} />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default ArtistUpdateMerchandise;
